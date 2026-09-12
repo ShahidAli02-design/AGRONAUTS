b4d3aa8021d0b78e7892b82954d11a93222522c4
Shahid ali <alishahid3789@gmail.com>
2026-09-12 11:48:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,23 +6,14 @@ import { defineConfig } from 'vite';
 export default defineConfig(() => {
   return {
     base: '/AGRONAUTS/',
-
     plugins: [react(), tailwindcss()],
-
     resolve: {
       alias: {
         '@': path.resolve(__dirname, 'src'),
-        '@tanstack/react-router': path.resolve(
-          __dirname,
-          'src/lib/router.tsx'
-        ),
-        '@tanstack/react-start': path.resolve(
-          __dirname,
-          'src/lib/agri.functions.ts'
-        ),
+        '@tanstack/react-router': path.resolve(__dirname, 'src/lib/router.tsx'),
+        '@tanstack/react-start': path.resolve(__dirname, 'src/lib/agri.functions.ts'),
       },
     },
-
     server: {
       hmr: process.env.DISABLE_HMR !== 'true',
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
